0d0ec662d78dff1f43a393ad7e8ef597d6d7b079
Samuel <samnmbuguah@gmail.com>
2024-07-28 20:13:41 +0300

    ✨ chore: Update TradingViewChart component to display crrypto instead of fx

--- Frontend/src/_components/TradingViewChart.jsx
+++ Frontend/src/_components/TradingViewChart.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, memo } from "react";
 
-export const Chart = memo((dark) => {
+export const Chart = memo(({ dark }) => {
   const container = useRef();
-  const darkMode = dark.dark;
+  const darkMode = dark;
 
   useEffect(() => {
     const colorTheme = darkMode ? "dark" : "light";
@@ -12,57 +12,55 @@ export const Chart = memo((dark) => {
       "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
     script.type = "text/javascript";
     script.async = true;
+    script.crossOrigin = "anonymous";
     script.innerHTML = `
-        {
-          "symbols": [
-            [ 
-              "OANDA:USDJPY|7D"
+{
+         "symbols": [
+            [
+              "BINANCE:BTCUSDT|1D"
             ],
             [
-              "OANDA:GBPJPY|7D"
+              "BINANCE:ETHUSDT|1D"
             ],
             [
-              "OANDA:USDMXN|7D"
+              "BINANCE:XRPUSDT|1D"
             ],
             [
-              "FX_IDC:EURMXN|7D"
-            ],
-            [
-              "OANDA:USDCHF|7D"
+              "BINANCE:ADAUSDT|1D"
             ]
           ],
-          "chartOnly": false,
-          "width": "100%",
-          "height": "100%",
-          "locale": "de_DE",
-          "colorTheme": "${colorTheme}",
-          "autosize": true,
-          "showVolume": false,
-          "showMA": false,
-          "hideDateRanges": false,
-          "hideMarketStatus": false,
-          "hideSymbolLogo": true,
-          "scalePosition": "right",
-          "scaleMode": "Logarithmic",
-          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-          "fontSize": "10",
-          "noTimeScale": false,
-          "valuesTracking": "1",
-          "changeMode": "price-and-percent",
-          "chartType": "area",
-          "maLineColor": "#2962FF",
-          "maLineWidth": 1,
-          "maLength": 9,
-          "backgroundColor": "${backgroundColor}",
-          "lineWidth": 2,
-          "lineType": 0,
-          "dateRanges": [
-            "1d|30",
-            "1w|60",
-            "1m|240",
-            "12m|1W"
-          ]
-        }`;
+  "chartOnly": false,
+  "width": "100%",
+  "height": "100%",
+  "locale": "en-US",
+  "colorTheme": "${colorTheme}",
+  "autosize": true,
+  "showVolume": false,
+  "showMA": true,
+  "hideDateRanges": false,
+  "hideMarketStatus": false,
+  "hideSymbolLogo": true,
+  "scalePosition": "right",
+  "scaleMode": "Logarithmic",
+  "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+  "fontSize": "10",
+  "noTimeScale": false,
+  "valuesTracking": "1",
+  "changeMode": "price-and-percent",
+  "chartType": "area",
+  "maLineColor": "#2962FF",
+  "maLineWidth": 1,
+  "maLength": 9,
+  "backgroundColor": "${backgroundColor}",
+  "lineWidth": 2,
+  "lineType": 0,
+  "dateRanges": [
+    "1d|30",
+    "1w|60",
+    "1m|240",
+    "12m|1W"
+  ]
+}`;
     container.current.innerHTML = "";
     container.current.appendChild(script);
   }, [darkMode]);
@@ -73,3 +71,5 @@ export const Chart = memo((dark) => {
     </div>
   );
 });
+
+export default Chart;
