56cc8edeab0cdcc76ede2c319fdceec393afc8a2
Samuel <samnmbuguah@gmail.com>
2024-07-28 20:11:18 +0300

    🐛 fix: Fix bug relating to userId and id while signing up

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -3,73 +3,72 @@ const db = require("../config/Database.js");
 
 const { DataTypes } = Sequelize;
 
-const Users = db.define('users', {
-    id: { 
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+const Users = db.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     userId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
     },
-    username:{
-        type: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
     },
-    password:{
-        type: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING,
     },
-    email:{
-        type: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
     },
-    account_no:{
-        type: DataTypes.STRING
+    account_no: {
+      type: DataTypes.STRING,
     },
     api_key: {
-        type: DataTypes.STRING(5000)
+      type: DataTypes.STRING,
     },
     api_secret: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
-    wallet:{
-        type: DataTypes.INTEGER
+    wallet: {
+      type: DataTypes.INTEGER,
     },
-    investment:{
-        type: DataTypes.INTEGER
+    investment: {
+      type: DataTypes.INTEGER,
     },
-    begin_date:{
-        type: DataTypes.DATEONLY
+    begin_date: {
+      type: DataTypes.DATEONLY,
     },
-    Net_client_share_in_percent:{
-        type: DataTypes.INTEGER
+    Net_client_share_in_percent: {
+      type: DataTypes.INTEGER,
     },
-    usertype:{
-        type: DataTypes.INTEGER
+    usertype: {
+      type: DataTypes.INTEGER,
     },
-    Admin_id:{
-        type: DataTypes.STRING
+    Admin_id: {
+      type: DataTypes.STRING,
     },
-    fee:{
-        type: DataTypes.DECIMAL(10, 2),
+    fee: {
+      type: DataTypes.DECIMAL(10, 2),
     },
     profit_now: {
-        type: DataTypes.DECIMAL(10, 2), 
+      type: DataTypes.DECIMAL(10, 2),
     },
     user_roles: {
-        type:DataTypes.STRING
-    }
-},{
-    freezeTableName:true
-});
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    freezeTableName: true,
+    hooks: {
+      afterCreate: (user, options) => {
+        user.userId = user.id;
+        return user.save();
+      },
+    },
+  }
+);
 
 module.exports = Users;
-
-
-
-
-
-
-
-
-
-
