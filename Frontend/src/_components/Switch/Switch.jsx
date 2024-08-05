6106c024905e0d212218d2c36ab0d4872760fe97
Samuel <samnmbuguah@gmail.com>
2024-08-05 18:24:30 +0300

    ✨ feat: Update Switch component to dispatch autoBot action with updated tradeData

--- Frontend/src/_components/Switch/Switch.jsx
+++ Frontend/src/_components/Switch/Switch.jsx
@@ -1,30 +1,39 @@
 import React, { useState } from 'react';
+import { autoBot } from "_store/bots.slice";
+import { useDispatch } from "react-redux";
 
-const Switch = ({ onChange }) => {
+
+const Switch = ({ onChange, tradeData }) => {
   const [isChecked, setIsChecked] = useState(false);
+  const dispatch = useDispatch();
 
   const handleCheckboxChange = () => {
     const newState = !isChecked;
     setIsChecked(newState);
     onChange(newState);
+
+    // Create a new tradeData object with the updated isChecked value
+    const updatedTradeData = { ...tradeData, active: newState };
+
+    dispatch(autoBot(updatedTradeData));
   };
 
   return (
-    <label className='themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center'>
+    <label className="themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={isChecked}
         onChange={handleCheckboxChange}
-        className='sr-only'
+        className="sr-only"
       />
       <span
         className={`slider flex h-5 w-[40px] items-center rounded-full p-1 duration-200 ${
-          isChecked ? 'bg-green-500' : 'bg-[#50555F]'
+          isChecked ? "bg-green-500" : "bg-[#50555F]"
         }`}
       >
         <span
           className={`dot h-4 w-4 rounded-full bg-white duration-200 ${
-            isChecked ? 'translate-x-[18px]' : ''
+            isChecked ? "translate-x-[18px]" : ""
           }`}
         ></span>
       </span>
