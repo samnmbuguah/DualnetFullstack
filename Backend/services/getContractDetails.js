6127ca1ffe7822d9e28289ee9bfb19bf4be5113b
Samuel <samnmbuguah@gmail.com>
2024-08-08 22:51:55 +0300

    ⚡️ feat: Update getContractDetails to use GateApi.FuturesApi for futures price retrieval

--- Backend/services/getContractDetails.js
+++ Backend/services/getContractDetails.js
@@ -1,33 +1,25 @@
-const GateApi = require('gate-api');
-const client = require('./gateClient');
+const GateApi = require("gate-api");
+const client = require("./gateClient.js");
+const api = new GateApi.FuturesApi(client);
 
-const futuresApi = new GateApi.FuturesApi(client);
+const defaultOpts = {
+  interval: "0", // string | Interval of order depth data
+  limit: 1, // number | Maximum number of order depth data in asks or bids
+  withId: false,
+};
 
-function getContractDetails(settle, contract) {
-    return futuresApi.listFuturesContracts(settle)
-        .then(response => {
-            const contractDetails = response.body.find(item => item.name === contract);
-            if (contractDetails) {
-                // Return the required contract details
-                return {
-                    name: contractDetails.name,
-                    lastPrice: contractDetails.lastPrice,
-                    markPrice: contractDetails.markPrice,
-                    indexPrice: contractDetails.indexPrice
-                };
-            } else {
-                console.log(`Contract ${contract} not found.`);
-                return null; // Return null if the contract is not found
-            }
-        })
-        .catch(error => {
-            console.error(error.response);
-            return null; // Return null if there's an error
-        });
-    
+async function getContractDetails(settle, contract, opts = defaultOpts) {
+  try {
+    const value = await api.listFuturesOrderBook(settle, contract, opts);
+    const firstAsk = parseFloat(value.body.asks[0].p);
+    return firstAsk;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 module.exports = getContractDetails;
 
-// // Usage:
-// getContractDetails('usdt', 'MOVEZ_USDT');
+// Usage:
+getContractDetails("usdt", "BTC_USDT");
