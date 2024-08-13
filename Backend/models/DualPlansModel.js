abc4f7c2a1082daa413bf6199d074c5781d9e026
Samuel <samnmbuguah@gmail.com>
2024-08-13 21:53:11 +0300

    🗃️ feat: Add DualPlansModel for managing dual investment plans

---
+++ Backend/models/DualPlansModel.js
@@ -0,0 +1,56 @@
+const { Sequelize } = require("sequelize");
+const db = require("../config/Database.js");
+
+const DualPlans = db.define(
+  "DualPlans",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    instrumentName: {
+      type: Sequelize.STRING,
+    },
+    investCurrency: {
+      type: Sequelize.STRING,
+    },
+    exerciseCurrency: {
+      type: Sequelize.STRING,
+    },
+    exercisePrice: {
+      type: Sequelize.FLOAT,
+    },
+    deliveryTime: {
+      type: Sequelize.INTEGER,
+    },
+    minCopies: {
+      type: Sequelize.INTEGER,
+    },
+    maxCopies: {
+      type: Sequelize.INTEGER,
+    },
+    perValue: {
+      type: Sequelize.STRING,
+    },
+    apyDisplay: {
+      type: Sequelize.STRING,
+    },
+    startTime: {
+      type: Sequelize.INTEGER,
+    },
+    endTime: {
+      type: Sequelize.INTEGER,
+    },
+    status: {
+      type: Sequelize.STRING,
+    },
+    planType: {
+      type: Sequelize.STRING,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = DualPlans;
