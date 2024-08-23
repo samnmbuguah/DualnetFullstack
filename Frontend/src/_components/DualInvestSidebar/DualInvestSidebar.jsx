52f97b0708964fb5b57d21188ef8d2f5c25cb7b7
Samuel <samnmbuguah@gmail.com>
2024-08-23 23:10:13 +0300

    💄 ✨ feat: Update DualInvestSidebar with DualTrade component and background image
    Refactor the DualInvestSidebar component to replace the DualInput component with the new DualTrade component. Additionally, add a background image to enhance the visual appeal of the sidebar.

--- Frontend/src/_components/DualInvestSidebar/DualInvestSidebar.jsx
+++ Frontend/src/_components/DualInvestSidebar/DualInvestSidebar.jsx
@@ -5,13 +5,14 @@ import styles from "./styles";
 import Switch from "_components/Switch/Switch";
 import cryptos from "./cryptos.json";
 import CryptoList from "./CryptoList";
-import DualInput from "./DualInput";
+import DualTrade from "./DualTrade";
 import Threshold from "./Threshold";
 import BuyLowInfo from "./ BuyLowInfo.jsx";
 import SellHighInfo from "./SellHighInfo";
 import { fetchInvestmentsByCurrency } from "_store/bots.slice";
 import BuyAPR from "./BuyAPR";
 import SellAPR from "./SellAPR";
+import backgroundImage from "../../_assets/chartCurve.svg";
 
 const DualInvestSidebar = ({ show, dark }) => {
   const { user: authUser } = useSelector((x) => x.auth);
@@ -46,61 +47,51 @@ const DualInvestSidebar = ({ show, dark }) => {
 
   return (
     <div
-      style={customStyles}
+      style={{
+        ...customStyles,
+      }}
       className={`${
         show ? "right-0" : "hidden"
       } bg-[#fef6e6] md:w-[${screenWidth}px] min-h-full px-4 py-3 z-30 transition-transform text-xs dark:bg-[transparent] dark:border-[#6D6D6D] bg-zinc-800 rounded-[25px] font-inter text-white flex flex-col justify-between`}
     >
-      <div>
-        <CryptoList
-          cryptos={cryptos}
-          selectedCrypto={selectedCrypto}
-          onCryptoClick={handleCryptoClick}
-        />
-        <div className="w-1/2 flex flex-row px-8">
-          <div className="mt-2">
-            <span className="mr-4 ">Dual-Invest auto on/off</span>
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: "left center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "auto",
+          margin: "0 4rem 0 1rem",
+        }}
+      >
+        <div>
+          <CryptoList
+            cryptos={cryptos}
+            selectedCrypto={selectedCrypto}
+            onCryptoClick={handleCryptoClick}
+          />
+          <div className="w-1/2 flex flex-row items-center px-8 py-4">
+            <span className="mr-4">Dual-Invest auto on/off</span>
             <Switch onChange={handleSwitchChange} tradeData={tradeData} />
           </div>
         </div>
-      </div>
-      <div className="flex flex-row">
-        <div className="w-1/3 py-24">
-          <div>
-            <span className="px-8 text-[#868585]">1 share = 0.2575 USDT</span>
-            <DualInput
-              label="Amount USDT"
-              onChange={handleAmountChange}
-              value={amount}
-              required={false}
-              labelColor="text-[#1D886A]"
+        <div className="flex flex-row ">
+            <DualTrade
+              buyLowPerShare="0.2575 USDT"
+              sellHighPerShare="0.0001 ETH"
+              currentPrice="58222"
             />
+          <div className="flex flex-col justify-between min-h-full">
+            <BuyLowInfo />
+            <SellHighInfo />
           </div>
           <div>
-            <span className="px-8">Current Price: $58222</span>
-          </div>
-          <div>
-            <DualInput
-              label="Amount BTC"
-              onChange={handleAmountChange}
-              value={amount}
-              required={false}
-              labelColor="text-[#F09643]"
-            />
-            <span className="px-8 text-[#868585]">1 share = 0.0001 ETH</span>
+            <BuyAPR items={exerciseCurrencyList} />
+            <SellAPR items={investCurrencyList} />
           </div>
         </div>
-        <div className="flex flex-col">
-          <BuyLowInfo />
-          <SellHighInfo />
+        <div className="w-3/5">
+          <Threshold />
         </div>
-        <div>
-          <BuyAPR items={exerciseCurrencyList} />
-          <SellAPR items={investCurrencyList} />
-        </div>
-      </div>
-      <div className="w-3/5">
-        <Threshold />
       </div>
     </div>
   );
