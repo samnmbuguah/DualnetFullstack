8be44696fbc06b537797f522ed6b651537ea2a5c
Samuel <samnmbuguah@gmail.com>
2024-08-23 23:09:33 +0300

    ✨ feat: Update Threshold component in DualInvestSidebar
    Refactor the Threshold component in the DualInvestSidebar to improve its layout and display. The changes include:
    - Adjusted the padding and margin to provide better spacing and alignment.
    - Updated the colors and font styles for better readability.
    - Added a new section for displaying the trigger point and open bots information.
    
    These changes aim to enhance the visual presentation and user experience of the Threshold component in the DualInvestSidebar.

--- Frontend/src/_components/DualInvestSidebar/Threshold.jsx
+++ Frontend/src/_components/DualInvestSidebar/Threshold.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 const Threshold = () => {
   return (
-    <div className="flex items-center justify-between p-2">
-      <span className="text-[#0336EB]">Hedge-Bot</span>
-      <span>Open Trigger = $58500</span>
-      <span className="text-[#0336EB]">Dual-Invest</span>
+    <div className="flex items-center justify-between p-8 mb-6">
+      <span className="text-[#1D8EFF] font-semibold italic text-sm">
+        Hedge-Bot
+      </span>
+      <div className="flex flex-col"> 
       <span>
-        APR to open &gt; <span>350</span>%
+        Triggerpoint = $<span>58500</span>
+      </span>
+      <span> Open bots 1 </span>
+      </div>
+      <span className="text-[#1D8EFF] font-semibold italic text-sm">
+        Dual-Invest
+      </span>
+      <span>
+        APR to open &gt; <span>150</span>%
       </span>
     </div>
   );
