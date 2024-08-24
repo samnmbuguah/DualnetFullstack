55ad0247208d12734fc5325087d9de10d2696e8d
Samuel <samnmbuguah@gmail.com>
2024-08-24 15:05:44 +0300

    🗃️ Refactor Dbsync.js to use "alter" instead of "force" for syncing DualPlans table

--- Backend/config/Dbsync.js
+++ Backend/config/Dbsync.js
@@ -31,7 +31,7 @@ Scans.sync()
   })
   .then(() => {
     console.log("TopScans table has been created.");
-    return DualPlans.sync({ force: true });
+    return DualPlans.sync({ alter: true });
   })
   .then(() => {
     console.log("All tables have been synced");
