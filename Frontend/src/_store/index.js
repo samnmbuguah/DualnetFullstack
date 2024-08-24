904cb03cda97f377072b45faca539e3172043e74
Samuel <samnmbuguah@gmail.com>
2024-08-24 11:01:23 +0300

    ✨ feat: Add dualsReducer to store configuration

--- Frontend/src/_store/index.js
+++ Frontend/src/_store/index.js
@@ -3,14 +3,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth.slice';
 import { usersReducer } from './users.slice';
 import { botsReducer } from './bots.slice';
+import { dualsReducer } from './duals.slice';
 
 export * from './auth.slice';
 export * from './users.slice';
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        users: usersReducer,
-        bots: botsReducer
-    },
+  reducer: {
+    auth: authReducer,
+    users: usersReducer,
+    bots: botsReducer,
+    duals: dualsReducer
+  },
 });
