77b217a7a7d6c1b5f06d3c31bf56a8a514d01392
Samuel <samnmbuguah@gmail.com>
2024-08-24 10:59:06 +0300

    ♻️ 💄 feat: Refactor BuyAPR component in DualInvestSidebar
    Refactor the BuyAPR component in the DualInvestSidebar to improve its functionality and user experience. The changes include:
    - Updated the rendering of the list items to display the exercise price, APR, and remaining time.
    - Added a "Subscribe" button for each item in the list.

--- Frontend/src/_components/DualInvestSidebar/BuyAPR.jsx
+++ Frontend/src/_components/DualInvestSidebar/BuyAPR.jsx
@@ -1,14 +1,14 @@
-import React, { memo } from "react";
+import React from "react";
 
 const BuyAPR = ({ items }) => {
-  console.log("Buy APR items", items);
-
   const calculateRemainingTime = (unixTime) => {
     const now = Date.now();
-    const timeDifference = unixTime * 1000 - now; // Convert unixTime to milliseconds
+    const timeDifference = unixTime * 1000 - now;
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    
+    const hours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+
     if (days > 1) {
       return `${days} Days`;
     } else if (days === 1) {
@@ -19,27 +19,23 @@ const BuyAPR = ({ items }) => {
   };
 
   return (
-    <div className="p-4 mb-4 bg-transparent text-white">
-      <ul className="mb-4">
-        {items
-          .slice(0, 6)
-          .map((item, index) => (
-            <li key={index} className="mb-2 flex items-center justify-between">
-              <div className="text-sm text-white">${item.exercisePrice}</div>
-              <div className="text-sm text-[#01D497]">
-                {parseFloat(item.apyDisplay).toFixed(2)}%
-              </div>
-              <div className="text-sm text-white">
-                {calculateRemainingTime(item.endTime)}
-              </div>
-              <button className="py-1 px-2 bg-[#353638] text-white rounded-xl hover:border-red-500 border">
-                Subscribe
-              </button>
-            </li>
-          ))}
-      </ul>
-    </div>
+    <ul className="mb-4 bg-transparent text-white text-shadow-inner text-sm font-medium rounded-none">
+      {items.slice(0, 6).map((item, index) => (
+        <li key={index} className="mb-4 flex items-center justify-between">
+          <span className="text-sm text-white mr-4">${item.exercisePrice}</span>
+          <span className="text-sm text-[#01D497]">
+            {parseFloat(item.apyDisplay).toFixed(2)}%
+          </span>
+          <span className="pl-4 text-sm text-white min-w-[84px]">
+            {calculateRemainingTime(item.endTime)}
+          </span>
+          <button className="py-1 px-2 ml-4 bg-[#353638] text-sm text-white rounded-xl hover:border-red-500 border shadow-md self-end">
+            Subscribe
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
-export default memo(BuyAPR);
+export default BuyAPR;
