af62b029f4f090b2ea73608c6382c40427ec9b1d
Samuel <samnmbuguah@gmail.com>
2024-08-24 10:58:15 +0300

    🗑️ ⚡️ feat: Update to remove default props and replace with javascript defaults

--- Frontend/src/_components/DualInvestSidebar/SellHighInfo.jsx
+++ Frontend/src/_components/DualInvestSidebar/SellHighInfo.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-function SellHighInfo({ infoText, availableAmount, positionAmount, positionRange }) {
+function SellHighInfo({
+  infoText = 'Sell High means choosing a target price higher than the current price to sell high and earn extra USDT.',
+  availableAmount = 0.0005987,
+  positionAmount = 100,
+  positionRange = 0.00001497
+}) {
   return (
     <section className="w-full max-w-[273px] p-4 mb-16"> 
       <h2 className="text-base font-bold text-orange-400">Sell High</h2>
@@ -24,12 +29,5 @@ function PositionSize({ amount, range }) {
     <p className="text-xs text-neutral-400">Position size for {amount}$ Range ({range.toFixed(8)})</p>
   );
 }
-
-SellHighInfo.defaultProps = {
-  infoText: 'Sell High means choosing a target price higher than the current price to sell high and earn extra USDT.',
-  availableAmount: 0.0005987,
-  positionAmount: 100,
-  positionRange: 0.00001497,
-};
 
 export default SellHighInfo;
