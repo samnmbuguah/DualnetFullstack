61db9c2f2736e97ba0e020eb8669515da771c4bb
Samuel <samnmbuguah@gmail.com>
2024-08-26 14:59:31 +0300

    💄 ✨ feat: Update BuyLowInfo component with opened duals count
    The BuyLowInfo component is updated to display the count of opened duals. This information is fetched from the backend and rendered in the component.

--- Frontend/src/_components/DualInvestSidebar/ BuyLowInfo.jsx
+++ Frontend/src/_components/DualInvestSidebar/ BuyLowInfo.jsx
@@ -3,30 +3,29 @@ import React from "react";
 function BuyLowInfo({
   infoText = 'Buy Low means choosing a target price lower than the current price and buying more crypto at a lower price.',
   availableAmount = 489.36,
-  positionAmount = 100,
-  positionRange = 122.34
+  openDuals = 0
 }) {
   return (
-    <section className="w-full max-w-[273px] p-4 mt-14">
+    <section className="w-full p-4 min-h-1/2">
       <h2 className="text-base font-bold text-teal-700">Buy Low</h2>
       <InfoText text={infoText} />
       <AvailableAmount amount={availableAmount} />
-      <PositionSize amount={positionAmount} range={positionRange} />
+      <OpenedDuals openDuals={openDuals} />
     </section>
   );
 }
 
 function InfoText({ text }) {
-  return <p className="text-xs text-stone-300 mb-4">{text}</p>;
+  return <p className="text-xs text-stone-300 mb-4 font-normal">{text}</p>;
 }
 
 function AvailableAmount({ amount }) {
-  return <p className="text-sm font-bold text-white">Available {amount.toFixed(2)}</p>;
+  return <p className="text-sm font-medium text-white">Available {amount.toFixed(2)}</p>;
 }
 
-function PositionSize({ amount, range }) {
+function OpenedDuals({ openDuals }) {
   return (
-    <p className="text-xs text-neutral-400">Position size for {amount}$ Range ({range.toFixed(2)}$)</p>
+    <p className="text-xs text-white font-normal">Opened Duals {openDuals}</p>
   );
 }
 
