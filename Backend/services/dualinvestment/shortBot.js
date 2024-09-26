b43d07ebc86f8fb4f88b03c97d5713efcadcb9c1
Samuel <samnmbuguah@gmail.com>
2024-09-26 10:02:37 +0300

    ✨ Use futures price instead of spot price in shortBot

--- Backend/services/dualinvestment/shortBot.js
+++ Backend/services/dualinvestment/shortBot.js
@@ -1,6 +1,6 @@
 const openShort = require("./openShort.js");
 const closeShort = require("./closeShort.js");
-const getSpotPrice = require("./getSpotPrice.js");
+const getFuturesPrice = require("./getFuturesPrice.js");
 const ShortBot = require("../../models/shortBotModel.js");
 
 async function manageShortBots() {
@@ -16,7 +16,7 @@ async function manageShortBots() {
     // Iterate over the fetched records and open short if trigger price is lower than current price
     if (shortBotsToOpen.length > 0) {
       for (const bot of shortBotsToOpen) {
-        const currentPrice = await getSpotPrice(bot.currency);
+        const currentPrice = await getFuturesPrice("usdt", bot.currency, { interval: '10s', limit: 1 });
         if (currentPrice !== null && currentPrice < bot.strikePrice) {
           await openShort("usdt", bot.currency, bot.size, bot.userId);
           await bot.update({ open: true });
@@ -36,7 +36,7 @@ async function manageShortBots() {
     // Iterate over the fetched records and close short if current price is higher than trigger price
     if (shortBotsToClose.length > 0) {
       for (const bot of shortBotsToClose) {
-        const currentPrice = await getSpotPrice(bot.currency);
+        const currentPrice = await getFuturesPrice("usdt", bot.currency, { interval: '10s', limit: 1 });
         if (currentPrice !== null && currentPrice > bot.strikePrice) {
           await closeShort(bot.currency, bot.size, bot.userId);
           await bot.update({ open: false });
