2b198e9336873c7eec78a70c19fa333f4fca1598
Samuel <samnmbuguah@gmail.com>
2024-09-26 11:22:17 +0300

    ✨ Activate arbitrage bot

--- Backend/index.js
+++ Backend/index.js
@@ -169,20 +169,20 @@ cron.schedule("*/10 * * * * *", async () => {
 //   timezone: "Etc/UTC"
 // });
 
-// cron.schedule('* * * * *', async () => {
-//     // Fetch all bots where isClose is false
-//     const bots = await Bots.findAll({ where: { isClose: false } });
-//     if (bots.length) {
-//         try {
-//             await closeByProfit(io, bots);
-//             console.log('Completed the Close By profit loop');
-//         } catch (error) {
-//             console.error('Error closing trades:', error);
-//         }
-//     }
-// });
+cron.schedule('* * * * *', async () => {
+    // Fetch all bots where isClose is false
+    const bots = await Bots.findAll({ where: { isClose: false } });
+    if (bots.length) {
+        try {
+            await closeByProfit(io, bots);
+            console.log('Completed the Close By profit loop');
+        } catch (error) {
+            console.error('Error closing trades:', error);
+        }
+    }
+});
 
-// cron.schedule('0 0 * * *', populateTables);
-// cron.schedule('0 */8 * * *', updateAccumulatedFunding);
-// cron.schedule('*/10 * * * *', updateFundingRate);
-// checkTrades();
+cron.schedule('0 0 * * *', populateTables);
+cron.schedule('0 */8 * * *', updateAccumulatedFunding);
+cron.schedule('*/10 * * * *', updateFundingRate);
+checkTrades();
