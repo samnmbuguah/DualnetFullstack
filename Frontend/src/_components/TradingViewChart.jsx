e18cda220327e9d99c790e4bdcef800b2ca722dc
Samuel <samnmbuguah@gmail.com>
2024-09-30 12:27:20 +0300

    ✨ update to Use coingecko chart instead of tradingview

--- Frontend/src/_components/TradingViewChart.jsx
+++ Frontend/src/_components/TradingViewChart.jsx
@@ -2,80 +2,28 @@ import React, { useEffect, useRef, memo } from "react";
 
 export const Chart = memo(({ dark }) => {
   const container = useRef();
-  const darkMode = dark;
 
   useEffect(() => {
-    if (!container.current) return;
+    const script = document.createElement('script');
+    script.src = "https://widgets.coingecko.com/gecko-coin-price-chart-widget.js";
+    script.async = true;
+    document.body.appendChild(script);
 
-    const colorTheme = darkMode ? "dark" : "light";
-    const backgroundColor = darkMode ? "rgba(33, 36, 41, 0.02)" : "#fef6e6";
-
-    const appendScript = () => {
-      const script = document.createElement("script");
-      script.src =
-        "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.crossOrigin = "anonymous";
-      script.innerHTML = `
-{
-         "symbols": [
-            [
-              "BINANCE:BTCUSDT|1D"
-            ],
-            [
-              "BINANCE:ETHUSDT|1D"
-            ],
-            [
-              "BINANCE:XRPUSDT|1D"
-            ],
-            [
-              "BINANCE:ADAUSDT|1D"
-            ]
-          ],
-  "chartOnly": false,
-  "width": "100%",
-  "height": "100%",
-  "locale": "en-US",
-  "colorTheme": "${colorTheme}",
-  "autosize": true,
-  "showVolume": false,
-  "showMA": true,
-  "hideDateRanges": false,
-  "hideMarketStatus": false,
-  "hideSymbolLogo": true,
-  "scalePosition": "right",
-  "scaleMode": "Logarithmic",
-  "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-  "fontSize": "10",
-  "noTimeScale": false,
-  "valuesTracking": "1",
-  "changeMode": "price-and-percent",
-  "chartType": "area",
-  "maLineColor": "#2962FF",
-  "maLineWidth": 1,
-  "maLength": 9,
-  "backgroundColor": "${backgroundColor}",
-  "lineWidth": 2,
-  "lineType": 0,
-  "dateRanges": [
-    "1d|30",
-    "1w|60",
-    "1m|240",
-    "12m|1W"
-  ]
-}`;
-      container.current.innerHTML = "";
-      container.current.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
     };
-
-    // Delay the script execution to ensure the DOM is fully ready
-    setTimeout(appendScript, 3000);
-  }, [darkMode]);
+  }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
+    <div ref={container} className="w-full h-full">
+      <gecko-coin-price-chart-widget 
+        locale="en" 
+        transparent-background="true" 
+        coin-id="bitcoin" 
+        initial-currency="usd" 
+        width="100%" 
+        height="100%"
+      />
     </div>
   );
 });
