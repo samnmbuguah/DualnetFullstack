41a6a8be701f202b615de2ebc2c3f404ba027c63
Samuel <samnmbuguah@gmail.com>
2024-09-30 16:37:07 +0300

    💄 Edit the header design

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 import power from "../_assets/logout-btn.png";
 
-export function Header({ username, onLogout, dark }) {
+export function Header({ username, onLogout }) {
   return (
-    <div className="flex w-full justify-between text-2xl md:mb-4 md:whitespace-nowrap items-center font-syncopate-light px-1">
-      <span className="lg:text-[35px] font-bold dark:text-neutral-400 ">
-        {username}
-      </span>
-
+    <div className="flex flex-row items-center">
+      <div className="flex-grow border-b-[0.3px] border-[#857F76] mb-3">
+        <span className="lg:text-[35px] font-bold dark:text-neutral-400 block w-full">
+          {username}
+        </span>
+      </div>
       <button
-        className="text-right dark:text-stone-300 text-[15px] font-bold lg:flex items-center hidden mb-1"
+        className="w-[36px] h-[40px] dark:text-stone-300 text-[15px] font-bold flex items-center mt-10 ml-4"
         onClick={onLogout}
       >
-        Logout
         <img width={30} src={power} alt="power" className="ml-1" />
       </button>
     </div>
