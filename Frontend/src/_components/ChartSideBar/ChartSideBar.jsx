f9320ee1a4dc10ef671e2a9b64695f18070ae288
Samuel <samnmbuguah@gmail.com>
2024-09-30 12:23:25 +0300

    ✨ Update to use Coingecko chart instead of tradingview

--- Frontend/src/_components/ChartSideBar/ChartSideBar.jsx
+++ Frontend/src/_components/ChartSideBar/ChartSideBar.jsx
@@ -1,25 +1,16 @@
 import { useWindowDimensions } from "_components";
-import { api_url, fetchWrapper } from "_helpers";
-import { fetchBotsByUser } from "_store/bots.slice";
-import useTopScansWebSocket from "_store/websocket/TopScans";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import styles from "./styles";
 
-const baseUrl = `${fetchWrapper.api_url}/api`;
-
-
-function ChartSideBar({ show, setShow, dark, width }) {
-  const { user: authUser } = useSelector((x) => x.auth);
-  const dispatch = useDispatch();
+function ChartSideBar({ show, dark, width }) {
   const customStyles = dark ? styles.dark : styles.light;
-  let screenWidth = useWindowDimensions().width;
+  const { width: screenWidth } = useWindowDimensions();
+
+  if (!show) return null;
 
   return (
     <div
       style={customStyles}
-      className={`${show ? "right-0" : "hidden"
-        } bg-[#fef6e6] md:w-[${width}px] h-full  px-5 py-3 z-30 transition-transform text-xs dark:bg-[transparent] dark:border-[#6D6D6D] bg-zinc-800 rounded-[25px]`}
+      className={`bg-[#fef6e6] md:w-[${width || screenWidth}px] h-full px-5 py-3 z-30 transition-transform text-xs dark:bg-[transparent] dark:border-[#6D6D6D] bg-zinc-800 rounded-[25px]`}
     >
       <div>Chart sidebar</div>
     </div>
