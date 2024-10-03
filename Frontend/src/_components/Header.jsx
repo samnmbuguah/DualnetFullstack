8b6f8c9ea16074cac7003f1131a9800922fc024f
Samuel <samnmbuguah@gmail.com>
2024-10-03 14:31:28 +0300

    ✨ Header Light mode

--- Frontend/src/_components/Header.jsx
+++ Frontend/src/_components/Header.jsx
@@ -7,19 +7,21 @@ import modeToggle from "_assets/newLogo.svg";
 export function Header({ isDarkMode, toggleDarkMode, showInfo, logout, isAuthRoute, screenWidth, authUser }) {
   if (isAuthRoute) return null;
 
+  const headerTextClass = `${styles.headerText} ${isDarkMode ? styles.darkModeText : styles.lightModeText}`;
+
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={isDarkMode ? darkLogo : lightLogo} alt="logo" />
       <div className={`${styles.headerNav}`}>
         <span className={styles.modeToggle} onClick={toggleDarkMode}>
-          <span className={styles.headerText}>MODE</span>
+          <span className={headerTextClass}>MODE</span>
           <img src={modeToggle} alt="mode-toggle" className={styles.toggleButton} />
         </span>
         <span className={styles.headerLink} onClick={() => showInfo(1)}>
-          <span className={styles.headerText}>WHITEPAPER</span>
+          <span className={headerTextClass}>WHITEPAPER</span>
         </span>
         <span className={styles.faq} onClick={() => showInfo(2)}>
-          <span className={styles.headerText}>FAQ</span>
+          <span className={headerTextClass}>FAQ</span>
         </span>
       </div>
     </header>
