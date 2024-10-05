b14dce92e9beddacc9a6957d04e40879a7de5404
Samuel <samnmbuguah@gmail.com>
2024-10-05 12:15:24 +0300

    💄 Fix dark mode

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -36,7 +36,7 @@ export function App() {
   return (
     <div
       className={`flex flex-col justify-center items-center w-full min-h-screen px-10 ${
-        isDarkMode ? "dark-appContainer" : "light-appContainer"
+        isDarkMode ? "dark dark-appContainer" : "light-appContainer"
       }`}
     >
       <Header
