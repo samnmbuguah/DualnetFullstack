30f1786847a3b137b31d51c0e364777b4f90fbdb
Samuel <samnmbuguah@gmail.com>
2024-10-05 12:27:19 +0300

    💄 Go your chart fix

--- Frontend/src/pages/UserManage.jsx
+++ Frontend/src/pages/UserManage.jsx
@@ -67,7 +67,7 @@ function Users({ setCurrentView, isDarkMode }) {
                     </div>
                     <NavLink to="/" className="flex items-center gap-3 font-syn-regular">
                         <img width={150} height={100} src={goYourChart} className={`inline-block ${isDarkMode ? 'hidden' : ''}`} alt="go to your chart" property="true" />
-                        <span className={`text-[12px] ${isDarkMode ? 'text-white' : 'hidden'}`}>Go to your Chart</span>{' '}
+                        <span className={`text-[12px] font-syn-regular ${isDarkMode ? 'text-white' : 'hidden'}`}>Go your Chart</span>{' '}
                         <img width={30} src={darkChart} className={isDarkMode ? 'inline-block' : 'hidden'} alt="chart" />
                     </NavLink>
                     <button className="ml-10 inline-flex items-center text-xs bg-gradient-to-r from-[#777] to-[#0094FF] from-10% to-100% text-transparent bg-clip-text" onClick={()=>logout()}>
