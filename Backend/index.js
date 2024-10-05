c7325fc6ee2a94905195f4aefde076996a043278
Samuel Nyambura <samnmbuguah@gmail.com>
2024-10-05 13:10:10 +0000

    ✨ Activate arbitrage bot deactivate autobot

--- Backend/index.js
+++ Backend/index.js
@@ -99,7 +99,7 @@ io.on("connection", (socket) => {
 
 server.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
-  // StreamPrices(io); // Start streaming prices after the server has started
+  StreamPrices(io); // Start streaming prices after the server has started
 });
 
 // Schedule the cron job to run listDualInvestmentPlans once every 1 minute
@@ -169,20 +169,20 @@ cron.schedule(
 //   timezone: "Etc/UTC"
 // });
 
-// cron.schedule('* * * * *', async () => {
-//     // Fetch all bots where isClose is false
-//     const bots = await Bots.findAll({ where: { isClose: false } });
-//     if (bots.length) {
-//         try {
-//             await closeByProfit(io, bots);
-//             console.log('Completed the Close By profit loop');
-//         } catch (error) {
-//             console.error('Error closing trades:', error);
-//         }
-//     }
-// });
+cron.schedule('* * * * *', async () => {
+    // Fetch all bots where isClose is false
+    const bots = await Bots.findAll({ where: { isClose: false } });
+    if (bots.length) {
+        try {
+            await closeByProfit(io, bots);
+            console.log('Completed the Close By profit loop');
+        } catch (error) {
+            console.error('Error closing trades:', error);
+        }
+    }
+});
 
-// cron.schedule('0 0 * * *', populateTables);
-// cron.schedule('0 */8 * * *', updateAccumulatedFunding);
-// cron.schedule('*/10 * * * *', updateFundingRate);
-// checkTrades();
+cron.schedule('0 0 * * *', populateTables);
+cron.schedule('0 */8 * * *', updateAccumulatedFunding);
+cron.schedule('*/10 * * * *', updateFundingRate);
+checkTrades();
--- Backend/services/autoBot.js
+++ Backend/services/autoBot.js
@@ -56,5 +56,3 @@ async function autoBot(
     return false;
   }
 }
-
-module.exports = autoBot;
